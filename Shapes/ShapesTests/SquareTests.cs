6e9af3a522ff3f60b62195fd2daf12b3458f54b5
Mike <mikeblack25@gmail.com>
2020-12-08 10:19:27 -0800

    Add xUnit test project to run behavior for Square class
    - The CalculateArea method will fail initially per test driven development (TDD).

---
+++ Shapes/ShapesTests/SquareTests.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace ShapesTests
+{
+    /// <summary>
+    /// Test behavior of Square.
+    /// </summary>
+    public class SquareTests
+    {
+        /// <summary>
+        /// Calculate the area of a Square.
+        /// </summary>
+        [Fact]
+        public void CalculateAreaTest()
+        {
+            Assert.False(true);
+        }
+    }
+}
