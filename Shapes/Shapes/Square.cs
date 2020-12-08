7adf15158a3425e52c8302e5570d831b5dc5a1b6
Mike <mikeblack25@gmail.com>
2020-12-08 09:58:21 -0800

    Add class implementation for Square
    - Add Length property and Area method.

--- Shapes/Shapes/Square.cs
+++ Shapes/Shapes/Square.cs
@@ -5,5 +5,18 @@
     /// </summary>
     public class Square
     {
+        /// <summary>
+        /// Size of the sqare size.
+        /// </summary>
+        public int Length { get; set; } // Example of a property.
+
+        /// <summary>
+        /// Calculate the area of the squre (length * length).
+        /// </summary>
+        /// <returns></returns>
+        public int Area() // Example of a method.
+        {
+            return Length * Length;
+        }
     }
 }
