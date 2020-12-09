d6337f039df98ccab8a6be49fef8fef17b0d923b
Matt <tailsfromthecryptic@gmail.com>
2020-12-08 18:07:32 -0800

    Add Rectangle shape w/ test

---
+++ Shapes/Shapes/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿namespace Shapes
+{
+    /// <summary>
+    /// A Rectangle is a shape that has 4 strait sides and 4 right angles.
+    /// </summary>
+    public class Rectangle
+
+    {
+        /// <summary>
+        /// Build a Rectangle object with a given length.
+        /// </summary>
+        /// <param name="length"></param>
+        /// <param name="height"></param>
+        public Rectangle(int length, int height) // Special type of method called a Constructor (same name as class).
+        {
+            Length = length;
+            Height = height;
+        }
+
+        /// <summary>
+        /// Size of the Rectangle size.
+        /// </summary>
+        public int Length { get; set; } // Example of a property.
+        public int Height { get; set; }
+        /// <summary>
+        /// Calculate the area of the Rectangle (length * length).
+        /// </summary>
+        /// <returns></returns>
+        public int Area() // Example of a method.
+        {
+            return Length * Height;
+        }
+    }
+}
