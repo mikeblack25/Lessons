bef557e98383ff8e74ade8cbb58dd7dc5caf19cb
Mike <mikeblack25@gmail.com>
2020-12-10 11:20:12 -0800

    Add RestSharp library for helping with REST requests
    - Update Request class with Get method to request data from a Git REST API.

--- API/GoogleAPI/Request.cs
+++ API/GoogleAPI/Request.cs
@@ -1,10 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using RestSharp;
 
 namespace GoogleAPI
 {
+    /// <summary>
+    /// Request information from a REST API.
+    /// </summary>
     public class Request
     {
+        /// <summary>
+        /// Build a Request object with the given baseAddress.
+        /// </summary>
+        /// <param name="baseAddress"><see cref="BaseAddress"/></param>
+        public Request(string baseAddress)
+        {
+            BaseAddress = baseAddress;
+        }
+
+        /// <summary>
+        /// Base address for the given requests.
+        /// </summary>
+        public string BaseAddress { get; }
+
+        /// <summary>
+        /// Request data from a Get REST API.
+        /// </summary>
+        /// <param name="route">Request path after the BaseAddress.</param>
+        /// <returns></returns>
+        public string Get(string route)
+        {
+            var client = new RestClient(BaseAddress);
+            var request = new RestRequest(route, Method.GET);
+
+            return client.Execute(request).Content;
+        }
     }
 }
